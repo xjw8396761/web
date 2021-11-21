8c83ef6460f0739f6fb467cad90992e1d7359536
yunzic_steven <xjw8396761@126.com>
2021-11-21 20:47:23 +0800

    delete over code

--- framework/core.go
+++ framework/core.go
@@ -12,18 +12,3 @@ func NewCore() *Core {
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	//TODO
 }
-package framework
-
-import "net/http"
-
-type Core struct {
-
-}
-
-func NewCore() *Core {
-	return &Core{}
-}
-
-func (c *Core)ServeHTTP(response http.ResponseWriter, request *http.Request){
- //Todo
-}
